2d2bc5d0c37c4826f26d1ef6c4df09077ce51174
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-03-12 13:49:48 +0100

    Test changement

--- pages/training/[language]/[slug]/index.vue
+++ pages/training/[language]/[slug]/index.vue
@@ -4,11 +4,13 @@ const params = route.params;
 const slug = params.slug;
 const lang = params.language;
 const training = await queryContent(`/training/${lang}/${slug}`).findOne();
+const categoryObject = await queryContent(`/category/${lang}/${training.category}`).findOne();
+
 </script>
 <template>
   <v-container>
     <ContentRenderer :value="training">
-      <b>Titre formation : </b>{{ training.title }}<br/>
+      <b>Titre formation : </b>{{ training.title }} {{ categoryObject.title }}<br/>
     </ContentRenderer>
   </v-container>
 </template>
