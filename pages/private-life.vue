24909c85761f521effa90094a68394f85ad9658a
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-04-11 14:17:49 +0200

    add private life content

--- pages/private-life.vue
+++ pages/private-life.vue
@@ -1,31 +1,55 @@
 <template>
-private life page 
-<v-container>
-        <v-list>
-          <v-list-item class="mb-2" v-for="(item, key) in categorys" :key="key">
-            <NuxtLink :to="item.to">
-              {{ item.title }}
-            </NuxtLink>
-          </v-list-item>
-        </v-list>
-      </v-container>
+  <h3 style="text-align: center;"> {{ $t('welcome') }}
+      Our general condition of sale</h3>
+  <main id="main" class="typography">
+      <MiscParseMarkdown :markdownString="markdown" />
+  </main>
+
 </template>
+
 
 <script setup lang="ts">
 
-  const locale = process.client ? navigator.language : '';
 
-  // browserLanguage qui fonctionne que en local mais pas sur netlify j'ai 
-  // du coup je peux pas remplacer le /fr/ par la langue du navigateur snif
-  // const browserLanguage = locale.substr(0, 2).toLowerCase(); // en-US to en
-  const categorys = await queryContent("/category/fr/").sort({ date: -1 }).find(); // fetch category
+const { locale, setLocale } = useI18n(); // for the translation
+
+console.log("locale; " + locale.value)
+
+var { data: generalCondition } = await reactive(await useAsyncData(`privatelife-${locale.value}/`, () =>
+  queryContent(`/privatelife/${locale.value}/private-life-2`).findOne())
+);
 
 
-  useHead({
-    script: [
-      { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
-    ],
-  });
+let markdown: string;
+markdown = generalCondition?.content
 
+
+// watcher when we change the language of the website we need to refresh also the content
+// like that we dont need to refresh the page !
+async function loadContent() { // not used
+  var { data: generalCondition } = await reactive(await useAsyncData("generalCondition", () =>
+      queryContent(`/generalcondition/${locale.value}/general-condition-of-sale-2`).findOne())
+  );
+  markdown = generalCondition?.content
+}
+
+
+
+
+
+console.log(generalCondition);
 
 </script>
+
+
+<style lang="scss" scoped>
+main {
+  display: grid;
+  justify-items: center;
+  align-items: center;
+
+  div {
+      max-width: 500px;
+  }
+}
+</style>
