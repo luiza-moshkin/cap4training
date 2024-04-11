4be1b403e9ca6b00abff582185357da47372fb65
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-04-11 09:56:16 +0200

    change name

--- pages/category/search/[category]/index.vue
+++ pages/category/search/[category]/index.vue
@@ -4,20 +4,20 @@
 
 const route = useRoute();
 const params = route.params;
-var category = params.category;
+var cat = params.category;
 
 // const lang = params.language;
 const lang = params.language;
 
 
-const formations = await queryContent(`/training/fr/`).where({ category: category }).sort({ date: -1 }).find();
+const formations = await queryContent(`/training/fr/`).where({ category: cat }).sort({ date: -1 }).find();
 // .where({ category.upper(): category.upper() })
 
 
 
 </script>
 <template>
-  Training list for category "{{ category }}"
+  Training list for category "{{ cat }}"
   <div class="mb-2" v-for="formation of formations">
     <NuxtLink :to="formation._path">
       Title: {{ formation.title }}<br />
