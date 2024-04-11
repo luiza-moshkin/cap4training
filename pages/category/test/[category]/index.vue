5b0e5c6c80a802179bbd1908b2f638e748b940e9
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-04-11 11:06:11 +0200

    test fix

---
+++ pages/category/test/[category]/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+
+
+const route = useRoute();
+const params = route.params;
+var cat = params.category;
+
+// const lang = params.language;
+
+
+//const { data } = await useAsyncData('get-document', () => queryContent(`/training/fr/`).where({ category: category }).sort({ date: -1 }).find())
+// .where({ category.upper(): category.upper() })
+
+
+import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
+const query: QueryBuilderParams = { path: '/training/fr/', where: [{ category: cat }], sort: [{ date: -1 }] }
+
+</script>
+<template>
+  Liste for {{ cat }}
+  <ContentList :query="query" v-slot="{ list }">
+    <div v-for="article in list" :key="article._path">
+      <h3>{{ article.title }}</h3>
+    </div>
+  </ContentList>
+
+</template>
