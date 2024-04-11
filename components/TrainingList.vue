e7164f0d1db1325561f11f3067f3f1a5863950d5
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-04-11 09:34:39 +0200

    try to fix

---
+++ components/TrainingList.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+
+const route = useRoute();
+const params = route.params;
+
+const props = defineProps({
+    category: {
+        type: String,
+        required: true,
+    }
+});
+
+var cate = props.category;
+
+// const lang = params.language;
+
+
+
+const formations = await queryContent('training', 'fr').where({ category: cate }).sort({ date: -1 }).find();
+// .where({ category.upper(): category.upper() })
+
+
+</script>
+<template>
+    Training list for category "{{ cate }}"
+    <div class="mb-2" v-for="formation of formations">
+        <NuxtLink :to="formation._path">
+            Title: {{ formation.title }}<br />
+        </NuxtLink>
+    </div>
+</template>
--- pages/category/[categoryy].vue
+++ pages/category/[categoryy].vue
@@ -4,22 +4,10 @@
 
 const route = useRoute();
 const params = route.params;
-var cate = params.categoryy;
 
-// const lang = params.language;
-
-
-
-const formations = await queryContent('training', 'fr').where({ category: cate }).sort({ date: -1 }).find();
-// .where({ category.upper(): category.upper() })
 
 
 </script>
 <template>
-  Training list for category "{{ cate }}"
-  <div class="mb-2" v-for="formation of formations">
-    <NuxtLink :to="formation._path">
-      Title: {{ formation.title }}<br />
-    </NuxtLink>
-  </div>
+  <TrainingList :category="params.categoryy"></TrainingList>
 </template>
