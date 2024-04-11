5f096adcec29a85fb5ac42c985e5b1a866b957d1
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-04-11 10:06:59 +0200

    try to fix

--- pages/category/[language]/[category]/index.vue
+++ pages/category/[language]/[category]/index.vue
@@ -4,20 +4,20 @@
 
 const route = useRoute();
 const params = route.params;
-var cat = params.category;
+var category = params.category;
 
 // const lang = params.language;
 const lang = params.language;
 
 
-const formations = await queryContent(`/training/fr/`).where({ category: cat }).sort({ date: -1 }).find();
+const formations = await queryContent(`/training/${lang}/`).where({ category: category }).sort({ date: -1 }).find();
 // .where({ category.upper(): category.upper() })
 
 
 
 </script>
 <template>
-  Training list for category "{{ cat }}"
+  Training list for category "{{ category }}"
   <div class="mb-2" v-for="formation of formations">
     <NuxtLink :to="formation._path">
       Title: {{ formation.title }}<br />
