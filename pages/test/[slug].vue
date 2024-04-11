7443262697fbc13820132398584d9b1daf498da5
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-04-11 17:08:09 +0200

    add test folder

---
+++ pages/test/[slug].vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+
+
+const route = useRoute();
+const params = route.params;
+const cat = params.slug || 'null'
+
+
+
+const formations = await queryContent('/training/fr/').where({ category: cat }).sort({ date: -1 }).find();
+// .where({ category.upper(): category.upper() })
+
+
+
+</script>
+<template>
+  Training list for category "{{ cat }}"
+  <div class="mb-2" v-for="formation of formations">
+    <NuxtLink :to="'/training/' + formation.category + '/' +$extractTitleFromPath(formation._path)">
+      Title: {{ formation.title }}<br />
+    </NuxtLink>
+  </div>
+</template>
